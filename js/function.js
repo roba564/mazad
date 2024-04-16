4ad8138b2ac502b8fe2737c50c06992e7543e1ea
roba564 <roba.zamly.2000@gmail.com>
2024-04-16 12:48:00 +0300

    first commit

---
+++ js/function.js
@@ -0,0 +1,97 @@
+
+$(function(){
+    $("#checked").click(function(){
+        $("#wt-radioholder").slideToggle("slow");
+        $("#wt-jobs").val();
+    });
+    /*
+    $(".wt-categorycontent").hover(function(){
+        $(".wt-categoryslidup").fadeToggle("slow");
+    });
+    
+    $(document).ready(function(){
+  $("#fi1").click(function(){
+    $("#a1").hide();
+    $(".wt-categoryslidup").fadeIn();
+
+  });
+  */
+});
+/*
+$(document).ready(function(){
+    $("#fi1").click(function(){
+      $("#wt-categoryslidup").fadeToggle();
+    });
+    */
+   $(document).ready(function(){
+    $("#fi1").click(function(){
+        $("#wt-categoryslidup1").slideToggle("slow");
+        
+    });
+});
+$(document).ready(function(){
+    $("#fi2").click(function(){
+        $("#wt-categoryslidup2").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $("#fi3").click(function(){
+        $("#wt-categoryslidup3").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $("#fi4").click(function(){
+        $("#wt-categoryslidup4").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $("#fi5").click(function(){
+        $("#wt-categoryslidup5").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $("#fi6").click(function(){
+        $("#wt-categoryslidup6").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $("#fi7").click(function(){
+        $("#wt-categoryslidup7").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $("#fi8").click(function(){
+        $("#wt-categoryslidup8").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $("wt-viewmore").click(function(){
+        $("#wt-fwidgetcontent1").slideToggle("slow");
+    });
+    /* info*/
+    $(".img1").click(function(){ 
+        $(".div1").toggle();
+        });
+    
+        $(".img2").click(function(){ 
+        $(".div2").toggle();
+        });
+        
+        $(".img3").click(function(){ 
+        $(".div3").toggle();
+        });
+        //Row2
+        $(".img4").click(function(){ 
+        $(".div4").toggle();
+        });
+    
+        $(".img5").click(function(){ 
+        $(".div5").toggle();
+        });
+         
+        $(".img6").click(function(){ 
+        $(".div6").toggle();
+        });
+        
+        $('[data-toggle="tooltip"]').tooltip();
+});
